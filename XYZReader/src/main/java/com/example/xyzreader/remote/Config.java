cfc80223bc07fd235c1344cbf4225c04e3a90739
Lucas <lucas@linard.tech>
2017-02-25 13:03:40 -0300

    init

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -10,8 +10,8 @@ public class Config {
         URL url = null;
         try {
             url = new URL("https://dl.dropboxusercontent.com/u/231329/xyzreader_data/data.json" );
-        } catch (MalformedURLException ignored) {
-            // TODO: throw a real error
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
         }
 
         BASE_URL = url;
